6d1432bd40538858defaba780a646c79dd9cf8fe
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-30 12:40:47 +0500

    product of two numbers

---
+++ product.js
@@ -0,0 +1,10 @@
+// Write a program to print the product of two numbers defining your own function.
+
+function product(x,y){
+    return x*y;
+}
+
+var x = 1;
+var y = 2;
+var z = product(x,y);
+console.log(z);
